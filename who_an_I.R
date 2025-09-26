315f486dc105bd5c86b92edbd8eb8a2d339cf680
19ADC99 <112981712+19ADC99@users.noreply.github.com>
2025-09-26 18:50:58 +0200

    Create who_an_I.R

---
+++ who_an_I.R
@@ -0,0 +1,15 @@
+library(cli)
+who_am_i = function(name){
+  cli::cli_text("👋 Hi, my name is {name}.")
+  Sys.sleep(time = 1.5)
+  cli::cli_text("👀 What motivates me is that my work can have positive impact in people's life.")
+  Sys.sleep(time = 1.5)
+  cli::cli_text("🌱 I'm learning about failing fast and derisking drug discovery projects.")
+  Sys.sleep(time = 1.5)
+  cli::cli_text("💊 I'm currently working on applying multi-omics integration for targets discovery and validation.")
+  Sys.sleep(time = 1.5)
+  cli::cli_text("💞 I'd love to collaborate with like-minded people.")
+  Sys.sleep(time = 1.5)
+  cli::cli_text("📫 Want to connect? Use the badges below...")
+}
+who_am_i("Andrea")
